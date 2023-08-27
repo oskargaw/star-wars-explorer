3ae044f016019d6e3d6ebbbe57f448a940305340
Oskar Gawlak <oskar.gawlak@gmail.com>
2023-08-27 18:06:54 +0200

    feat: change title and description metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Star Wars Explorer</title>
+        <meta
+          name="description"
+          content="Star Wars explorer with info about all characters"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="p-24">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const orbitron = Orbitron({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${orbitron.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between ${orbitron.className}`}
     >
       Star Wars Explorer
     </main>
