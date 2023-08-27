32b9bc17b8ae0d35506cdc3c67d54d7fe605ab21
Oskar Gawlak <oskar.gawlak@gmail.com>
2023-08-27 17:17:11 +0200

    feat: change background and font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Inter } from 'next/font/google'
+import { Orbitron } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const orbitron = Orbitron({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${orbitron.className}`}
     >
       Star Wars Explorer
     </main>
